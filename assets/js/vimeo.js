f1a7a081e11aeba2caef32e2b000af61f4ffb8b7
aydee <aydee.qm@gmail.com>
2017-09-26 08:12:07 -0500

    conversion a clases

--- assets/js/vimeo.js
+++ assets/js/vimeo.js
@@ -10,17 +10,15 @@ class Playlist {
             searchTerm: "iPhone X",
         }
 
+        $('#searchbtn').click(()=>{
+            let searchTerm = $('#term-vimeo').val();
+            console.log(searchTerm);
+            this.youtubeSearch(searchTerm);
+         });
+
     }
 
-    init() {
-        this.youtubeSearch('iPhone X');
-        this.setup();
-    }
-    setup() {
-        $('#searchbtn').click(this.youtubeSearch);
-    }
-
-    getVideoList(videos) {
+    getVideoList (videos) {
         return videos.map((video, index) => {
             const url = `https://www.youtube.com/embed/${video.id.videoId}`;
             return `<div class='item'> 
@@ -44,7 +42,7 @@ class Playlist {
 
     youtubeSearch(searchTerm) {
         $("#root").empty();
-        searchTerm = $('#term-vimeo').val();
+        $('#details').empty();
         console.log(searchTerm);
         YTSearch({ key: API_KEY, term: searchTerm }, data => {
             console.log("result", data);
@@ -53,7 +51,7 @@ class Playlist {
                 selectedVideo: data[0],
                 searchTerm: searchTerm
             };
-            var list = this.getVideoList(this.result.videos);
+            let list = this.getVideoList(this.result.videos);
             let firstVideo = this.allVideo(this.result.selectedVideo);
             let firstDetail = this.getDetails(this.result.selectedVideo);
             console.log("lis: ", list);
@@ -66,12 +64,12 @@ class Playlist {
     videoSearch(searchTerm) {
         jQuery.getJSON("list.json", data => {
             console.log("result", data.items);
-            app.result = {
+            this.result = {
                 videos: data.items,
                 selectedVideo: data.items[0],
                 searchTerm: searchTerm
             };
-            var list = app.getVideoList(app.result.videos);
+            var list = this.getVideoList(this.result.videos);
             console.log("lis: ", list);
             $("#root").append(list);
         });
@@ -80,92 +78,4 @@ class Playlist {
 }
 
 let play = new Playlist();
-play.init();
 
-
-
-
-
-
-
-
-
-
-
-
-/* let app = {
-    result: {
-        videos: [],
-        selectedVideo: null,
-        searchTerm: "iPhone X",
-    },
-
-    init: function () {
-        app.youtubeSearch('iPhone X');
-        app.setup();
-    },
-
-    setup: function () {
-        $('#searchbtn').click(app.youtubeSearch);
-    },
-
-    getVideoList: function (videos) {
-        return videos.map((video, index) => {
-            
-            const url = `https://www.youtube.com/embed/${video.id.videoId}`;
-            return `<div class='item'> 
-                        <iframe class="embed-responsive-item" src=${url}> </iframe>
-                     </div>`;
-        });
-    },
-
-    allVideo: function (video) {
-        const url = `http://www.youtube.com/embed/${video.id.videoId}`;
-        return `<iframe class="embed-responsive-item" src=${url}> </iframe>`;
-    },
-
-    getDetails: function (video){
-        const titleUrl = video.snippet.title;
-        const description = video.snippet.description;
-        return ` <p class="media-object"><strong>${titleUrl}</strong></p>
-                    <p>${description}</p>`;
-                   
-  }, 
-
-    youtubeSearch: function (searchTerm) {
-        $("#root").empty();
-        searchTerm = $('#term-vimeo').val();
-        console.log(searchTerm);
-        YTSearch({ key: API_KEY, term: searchTerm }, data => {
-            console.log("result", data);
-            app.result = {
-                videos: data,
-                selectedVideo: data[0],
-                searchTerm: searchTerm
-            };
-            var list = app.getVideoList(app.result.videos);
-            let firstVideo = app.allVideo(app.result.selectedVideo);
-            let firsDetail = app.getDetails(app.result.selectedVideo);
-            console.log("lis: ", list);
-            $("#videoarea").append(firstVideo);
-            $('#details').append(firsDetail);
-            $("#root").append(list);
-        });
-    },
-
-    videoSearch: function (searchTerm) {
-        jQuery.getJSON("list.json", data => {
-            console.log("result", data.items);
-            app.result = {
-                videos: data.items,
-                selectedVideo: data.items[0],
-                searchTerm: searchTerm
-            };
-            var list = app.getVideoList(app.result.videos);
-            console.log("lis: ", list);
-            $("#root").append(list);
-        });
-    }
-}; */
-
-//$(document).ready(app.init);
